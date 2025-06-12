36a93a9a0acd0c1f58c7085780df209430a3a839
Maxi <maxteel1989@gmail.com>
2025-06-12 11:15:43 -0300

    portafolio-version1

---
+++ scritp.js
@@ -0,0 +1,150 @@
+/*
+document.getElementById("cambiar_color").addEventListener("click",function(){
+   
+    document.body.style.backgroundColor ="red";});
+*/
+
+document.getElementById("ocultar_boton").addEventListener("click",function(){
+    document.getElementById("demo").style.display ="none";
+    document.getElementById("text1").style.textAlign ="center";
+    document.getElementById("text1").style.color ="white";
+  
+   });
+
+
+//boton volver imagen
+document.getElementById("text").addEventListener("click",function(){
+    document.getElementById("demo").style.display ="block";
+   
+});
+/*
+document.getElementById("cambiar_tamaño").onclick = function(){
+    document.getElementById("demo1").innerHTML ="MIS DATOS PERSONALES ";}
+*/
+//*evento cambiar tamaño de letra parrafo*/
+/*BOTONES DE AUMENTAR TAMAÑO ++*/
+    document.getElementById("cambiar_tamaño").addEventListener("click",function(){
+       document.getElementById("demo1").style.fontSize="20px";       
+       });
+
+      document.getElementById("c-t+").addEventListener("click",function(){
+        document.getElementById("btn").style.fontSize="20px";       
+        });
+
+
+            document.getElementById("cambiar_tamaño3").addEventListener("click",function(){
+                document.getElementById("demo3").style.fontSize="20px";       
+                });
+
+                document.getElementById("cambiar_tamaño4").addEventListener("click",function(){
+                    document.getElementById("demo4").style.fontSize="20px";       
+                    });
+
+                    document.getElementById("cambiar_tamaño5").addEventListener("click",function(){
+                        document.getElementById("demo5").style.fontSize="20px";       
+                        });
+
+                        document.getElementById("cambiar_tamaño6").addEventListener("click",function(){
+                            document.getElementById("demo6").style.fontSize="20px";       
+                            });
+
+                            document.getElementById("cambiar_tamaño7").addEventListener("click",function(){
+                                document.getElementById("demo7").style.fontSize="20px";       
+                                });
+
+                                document.getElementById("cambiar_tamaño8").addEventListener("click",function(){
+                                    document.getElementById("demo8").style.fontSize="20px";       
+                                    });
+
+                                    document.getElementById("cambiar_tamaño9").addEventListener("click",function(){
+                                        document.getElementById("demo9").style.fontSize="20px";       
+                                        });
+
+
+//*evento volver letra original
+/*BOTONES DE BAJAR TAMAÑO ++*/
+document.getElementById("volver_tamaño").addEventListener("click",function(){
+document.getElementById("demo1").style.fontSize="15px";       
+});
+
+document.getElementById("c-t-").addEventListener("click",function(){
+    document.getElementById("btn").style.fontSize="15px";       
+    });
+
+    document.getElementById("volver_tamaño3").addEventListener("click",function(){
+        document.getElementById("demo3").style.fontSize="15px";       
+        });
+ 
+        document.getElementById("volver_tamaño4").addEventListener("click",function(){
+         document.getElementById("demo4").style.fontSize="15px";       
+         });
+
+         document.getElementById("volver_tamaño5").addEventListener("click",function(){
+            document.getElementById("demo5").style.fontSize="15px";       
+            });
+
+            document.getElementById("volver_tamaño6").addEventListener("click",function(){
+                document.getElementById("demo6").style.fontSize="15px";       
+                });
+
+                document.getElementById("volver_tamaño7").addEventListener("click",function(){
+                    document.getElementById("demo7").style.fontSize="15px";       
+                    });
+
+                    document.getElementById("volver_tamaño8").addEventListener("click",function(){
+                        document.getElementById("demo8").style.fontSize="15px";       
+                        });
+
+                        document.getElementById("volver_tamaño9").addEventListener("click",function(){
+                            document.getElementById("demo9").style.fontSize="15px";       
+                            });
+
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*//*
+//*evento cambiar fondo 
+document.getElementById("cambiar_color").addEventListener("click",function(){
+document.getElementById("color_fondo").style.backgroundColor ="white";       
+});
+//*evento volver fondo
+document.getElementById("volver_color").addEventListener("click",function(){
+document.getElementById("color_fondo").style.backgroundColor ="  rgb(151, 151, 151)";       
+});
+//* cambiar color verde verdadero (evento mouseover)
+document.getElementById("cajatexto").addEventListener("mouseover",function(){
+    document.getElementById("cajatexto").style.backgroundColor ="  rgb(138, 246, 105)";   
+    document.getElementById("cajatexto1").style.backgroundColor ="  rgb(138, 246, 105)";   
+
+    });
+//*cambiar color rojo (evento mouseover)
+document.getElementById("cajatexto2").addEventListener("mouseover",function(){
+document.getElementById("cajatexto2").style.backgroundColor =" rgb(241, 96, 96)";   
+document.getElementById("cajatexto3").style.backgroundColor =" rgb(241, 96, 96)";   
+
+});*/
+
+//*alerta boton enviar
+document.getElementById("alert").addEventListener("click",function(){
+alert("MUCHAS GRACIAS POR VER MI C.V !!!!") ;      
+});
+
+
+
+
+
+
+
+
+
+/*evento cambiar color de la pantalla js.
+document.getElementById("cambiar_color").addEventListener("click",function(){
+document..style.backgroundColor ="#ff0000";
+
+});
+*/
+
+
+
